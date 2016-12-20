f7b56d8f477987644b2711cdb5690f4532c41495
Gregory <greghojob@gmail.com>
2016-12-20 15:19:10 +0800

    First release.

---
+++ src/main/java/linebot/EchoApplication.java
@@ -0,0 +1,34 @@
+package linebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+/**
+ * Created by Gregory on 2016/12/20.
+ */
+@SpringBootApplication
+@LineMessageHandler
+public class EchoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoApplication.class, args);
+    }
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+}
