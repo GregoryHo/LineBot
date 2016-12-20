a4bbf87472c38c8aaff24186951390383f58bf9c
Gregory <greghojob@gmail.com>
2016-12-20 16:09:43 +0800

    Version 1.0.2

--- src/main/java/linebot/EchoApplication.java
+++ src/main/java/linebot/EchoApplication.java
@@ -24,25 +24,33 @@ public class EchoApplication {
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         String message = event.getMessage().getText();
+        String userId = event.getSource().getUserId();
         String reply = null;
-        if (message.contains("代名詞")) {
-            if (message.contains("冠儀")) {
-                reply = "帥、帥、帥";
-            } else if (message.contains("雨晨")) {
-                reply = "溫柔體貼，美麗大方";
-            }
-        }
 
-        if (message.equals("尖叫聲")) {
-            reply = "啊啊啊啊啊啊啊!!!";
-        } else if (message.contains("我是說在座的各位都是")) {
-            reply = "垃圾";
-        } else if (message.contains("穩誠的專長是")) {
-            int random = (int) (Math.random() * 2);
-            if (random % 2 == 0) {
-                reply = "糗捏捏?";
-            } else {
-                reply = "糗口口口的捏捏?";
+        if (userId.equals("Ufae30cccfc1ca2c6045caad87e19fd2f")) {
+            if (message.contains("代名詞")) {
+                if (message.contains("冠儀")) {
+                    reply = "帥、帥、帥";
+                } else if (message.contains("雨晨")) {
+                    reply = "溫柔體貼，美麗大方";
+                }
+            }
+
+            if (message.equals("尖叫聲")) {
+                reply = "啊啊啊啊啊啊啊!!!";
+            }
+
+            if (message.contains("我是說在座的各位都是")) {
+                reply = "垃圾";
+            }
+        } else {
+            if (message.contains("穩誠的專長是")) {
+                int random = (int) (Math.random() * 2);
+                if (random % 2 == 0) {
+                    reply = "糗捏捏?";
+                } else {
+                    reply = "糗口口口的捏捏?";
+                }
             }
         }
 
